f1370e87306a7f00a2c1098d4f6f4d530a12cb87
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-07-16 17:40:35 -0500

    added struct and hashing function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,16 +3,27 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
   end
+
+  def build_grid(image) do
+
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] = image}) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
 end
