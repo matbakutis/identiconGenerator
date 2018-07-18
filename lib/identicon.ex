dae7d6305c96216ce4858b477e33778dd7b2b3d3
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-07-18 09:18:03 -0500

    fixed filter function, everything works as expected now

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -44,10 +44,10 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code, _index}) ->
+    filtered_grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
     end
-    %Identicon.Image{image | grid: grid}
+    %Identicon.Image{image | grid: filtered_grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
